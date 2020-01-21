10f779ba2ee1b83e486eb7e2a6c3d13fa08574a6
Victoria Aseff <vickase.a@gmail.com>
2020-01-21 12:54:17 -0300

    addEvent included

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -21,13 +21,16 @@ const Card = props => {
           info={props.info}
         />
         <div className="plus-container">
-        {" "}
-        <Plus className="plus" src={Plus} />
-      </div>
+          {" "}
+          <Plus className="plus" src={Plus} onClick={clickear} />
+        </div>
       </section>
-      
     </div>
   );
 };
 
 export default Card;
+
+const clickear = event => {
+  event.currentTarget.parentElement.parentElement.classList.toggle("agrandar");
+};
