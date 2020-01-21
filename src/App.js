319c701f2897771bb449c46f17fb5a2fad05fe5e
Victoria Aseff <vickase.a@gmail.com>
2020-01-21 11:15:33 -0300

    Info-section hover

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import imageSrcWonder from "./images/Wonder_Woman-large.jpg";
 
 const wonderWoman = {
   img: imageSrcWonder,
-  title: "asdasd",
-  subtitle: "qqqqq",
-  info: "LA INFOOOOOOO"
+  title: "Wonder Woman",
+  subtitle: "Powerful Women",
+  info:
+    "Diana Prince is the most recognizable female superhero in the world.Her debut film Wonder Woman is the highest grossing superhero origin film of all time. Created in 1941 and beloved for over 76 years, Diana Prince is a strong, compassionate role model for men and women everywhere."
 };
 
 function App() {
--- src/components/Card/WomenInfo/index.jsx
+++ src/components/Card/WomenInfo/index.jsx
@@ -1,25 +1,6 @@
 import React from "react";
 import "./index.css";
-/* 
-const PowerfulWomenInfo = (props) => {
-  return (
-    <div>
-      <header className="title">
-        <p className="sub">Powerful Women</p>
-        <h2 className="H2">Wonder Woman</h2>
-      </header>
-      <article className="articulo">
-        <p className="card-content">
-          Diana Prince is the most recognizable female superhero in the world.
-          Her debut film Wonder Woman is the highest grossing superhero origin
-          film of all time. Created in 1941 and beloved for over 76 years, Diana
-          Prince is a strong, compassionate role model for men and women
-          everywhere.
-        </p>
-      </article>
-    </div>
-  );
-}; */
+
 
 const PowerfulWomenInfo = props => {
   return (
